e3546b296af8da2e006a6a4fc437612abd1a59f4
dat25134 <tlvdat.bkdn@gmail.com>
2025-04-01 15:25:10 +0700

    fix sidebar and logo project

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -17,7 +17,7 @@ export default function Dashboard({ auth }) {
             <Head title="Dashboard" />
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div className="w-full">
                     {/* Welcome Message */}
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-6 p-6">
                         <div className="flex items-center justify-between">
