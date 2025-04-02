8937ad762b7ba4af6f3a39b9859fe4083befdfe7
dat25134 <tlvdat.bkdn@gmail.com>
2025-04-02 17:49:39 +0700

    fixbug/eslint_and_add_eslint_ci

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -2,31 +2,32 @@ import { useState, useEffect } from 'react';
 import Navbar from '@/Components/UI/Navbar';
 import Sidebar from '@/Components/UI/Sidebar';
 
-export default function Authenticated({ header, children }) {
+export default function Authenticated({ header, children, user }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
-    const [isSidebarOpen, setIsSidebarOpen] = useState(() => {
-        const saved = localStorage.getItem('sidebarOpen');
+    const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(() => {
+        const saved = localStorage.getItem('sidebarCollapsed');
         return saved ? JSON.parse(saved) : false;
     });
 
     useEffect(() => {
-        localStorage.setItem('sidebarOpen', JSON.stringify(isSidebarOpen));
-    }, [isSidebarOpen]);
+        localStorage.setItem('sidebarCollapsed', JSON.stringify(isSidebarCollapsed));
+    }, [isSidebarCollapsed]);
 
     return (
         <div className="min-h-screen bg-gray-100">
             <Navbar
+                user={user}
                 showingNavigationDropdown={showingNavigationDropdown}
                 setShowingNavigationDropdown={setShowingNavigationDropdown}
-                isSidebarOpen={isSidebarOpen}
+                isSidebarCollapsed={isSidebarCollapsed}
             />
             <Sidebar
-                isCollapsed={isSidebarOpen}
-                setIsCollapsed={setIsSidebarOpen}
+                isCollapsed={isSidebarCollapsed}
+                setIsCollapsed={setIsSidebarCollapsed}
             />
 
             {/* Main Content */}
-            <div className={`flex flex-1 flex-col transition-all duration-300 ${isSidebarOpen ? 'md:pl-16' : 'md:pl-64'}`}>
+            <div className={`flex flex-1 flex-col transition-all duration-300 ${isSidebarCollapsed ? 'md:pl-16' : 'md:pl-64'}`}>
                 {header && (
                     <header className="bg-white shadow">
                         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">{header}</div>
